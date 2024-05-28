450212c8170d261e7432cc04e9d325b1a7ced4ec
Putranugroho14 <putranugroho61@gmail.com>
2024-05-28 10:35:22 +0700

    membuat class base class

---
+++ overridding/overridding.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahkan final sesudah nama class
+//untuk mencegah adanya overriding
+class baseClass
+{
+public:
+	virtual void perkenalan()
+	{
+		cout << " Hallo saya function dari base class";
+	}
+};
+
+int main()
+{
+  
+}
+
