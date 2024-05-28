66c740424410bd2e73933d6bac2d9de3979cf194
Putranugroho14 <putranugroho61@gmail.com>
2024-05-28 10:14:31 +0700

    membuat command

--- paradigmaooppart2/paradigmaooppart2.cpp
+++ paradigmaooppart2/paradigmaooppart2.cpp
@@ -4,7 +4,12 @@ using namespace std;
 class seseorang
 {
 public:
+    // pure virtual function
     virtual void pesan() = 0;
+    //virtual void pesan()
+    //{
+    //   cout << "pesan dari seseorang" << endl;
+    //}
 };
 
 int main()
